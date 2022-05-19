f7e64050450592aaa2ce2bea3c93183c5f33b729
Markus <MarkusBertram@marbertram96@gmail.com>
2022-05-19 14:32:54 +0200

    added germeval2018 pipeline

---
+++ pipelines/germeval2018_pipeline.py
@@ -0,0 +1,45 @@
+import json
+import pickle
+import csv
+
+data_path = "../data/HS_Dataset1/"
+
+train = data_path + "HS_Dataset1_train.txt"
+
+test = data_path + "HS_Dataset1_test.txt"
+
+#unlabeled = data_path + "HS_Dataset1_unlabeled.txt"
+
+def read_file(filename):
+    read_data=list()
+
+    with open(filename, 'r', encoding='latin-1') as file:
+        reader=csv.reader(file, delimiter='\t')
+
+        for row in reader:
+            entry=dict()
+            entry['text'] = row[0]
+            entry['label_task1'] = row[1]
+            entry['label_task2'] = row[2]
+            read_data.append(entry)
+
+    return read_data
+    
+    
+    
+def get_data():
+    full_data = dict()
+    
+    # get train
+    read_data = read_file(train)
+    full_data["train"] = read_data
+
+    # get test
+    read_data = read_file(test)
+    full_data["test"] = read_data
+
+    ## get unlabeled
+    #read_data = read_file(unlabeled)
+    #full_data["unlabeled"] = read_data
+
+    return full_data
