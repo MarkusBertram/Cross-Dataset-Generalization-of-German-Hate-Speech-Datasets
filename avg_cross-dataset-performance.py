a2ed43b9e8f47613bd7feb92b0c97a157f687fe6
Markus <MarkusBertram@marbertram96@gmail.com>
2022-06-17 14:58:00 +0200

    first try of avg cross dataset performance

--- avg_cross-dataset-performance.py
+++ avg_cross-dataset-performance.py
@@ -148,7 +148,7 @@ def transform_to_dataset(dataset, tokenizer):
 
     return tensordataset
 
-def plotMatrix(eval_metrics,labels,results_dir, selected_type='f1', type_name=""):
+def plotMatrix(number_of_runs, eval_metrics,labels,results_dir, selected_type='f1', type_name=""):
     # now = datetime.now()
     # results_dir = "./results/"+"cross-dataset_performance_"+now.strftime("%Y%m%d-%H%M%S")+"/"
     # if os.path.exists(results_dir) == False:
@@ -156,24 +156,32 @@ def plotMatrix(eval_metrics,labels,results_dir, selected_type='f1', type_name=""
     #path_fig = "./results/"+strftime("%Y%m%d", gmtime())+ "-" + "-".join(labels).replace(" ","_")
     path_fig = results_dir
     sns.set(font_scale=1.0)
-    matrix = np.empty([len(eval_metrics),len(eval_metrics)])
-    for i in range(len(eval_metrics)):
-        for j in range(len(eval_metrics[i][selected_type])-1):
-            matrix[i][j] = eval_metrics[i][selected_type][j]
 
-    # calculate averages
-    avg_classifiers = []
-    avg_testsets = []
+    average_matrix = np.empty([len(eval_metric),len(eval_metric)])
+    avg_of_avg_classifiers = []
+    for eval_metric in eval_metrics:
 
-    for i in range(len(eval_metrics)):
-        avg_classifiers.append(eval_metrics[i][selected_type][-1])   
+        matrix = np.empty([len(eval_metric),len(eval_metric)])
+        for i in range(len(eval_metric)):
+            for j in range(len(eval_metric[i][selected_type])-1):
+                matrix[i][j] = eval_metric[i][selected_type][j]
+        average_matrix.append(matrix)
+        # calculate averages
+        avg_classifiers = []
+        avg_testsets = []
+        size = len(matrix[0])
+        for i in range(len(eval_metric)):
+            avg_classifiers.append(np.asarray(eval_metric[i][selected_type][-1]).reshape(size,1))
+        avg_of_avg_classifiers.append(avg_classifiers)
 
-
-    size = len(matrix[0])
+    # averaging of the runs:
+    matrix = np.mean(average_matrix, axis = 0)
+    avg_classifiers = np.mean(avg_of_avg_classifiers, axis = 0)
+    
     min_val = np.amin(matrix)
     max_val = np.amax(matrix) 
 
-    avg_classifiers = np.asarray(avg_classifiers).reshape(size,1)
+    #avg_classifiers = np.asarray(avg_classifiers).reshape(size,1)
 
     fig = plt.figure(figsize=(11,13))
     ax1 = plt.subplot2grid((10,9), (0,0), colspan=6, rowspan=7)
@@ -217,10 +225,10 @@ if __name__ == '__main__':
     SPLIT_RATIO = 0.2
     COMBINED_RATIO = 0.5
     model_name= 'deepset/gbert-base'
-    path = './tmp2/'
+    path = './tmp3/'
     number_of_tokens = 50
     batch = 10
-    num_epochs = 3
+    num_epochs = 6
     accelerator = Accelerator()
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     device = accelerator.device
@@ -251,130 +259,130 @@ if __name__ == '__main__':
         size_train = 1-SPLIT_RATIO
         size_test = SPLIT_RATIO
 
-    # split data into train and test set  
-    training_sets = []
-    validation_sets = []
-    test_sets = []
-    combined_test_set = None
-    for i,dataset in enumerate(data_sets):
-        ds_dict = {}
-        ds_dict_1 = {}
-        ds_dict_2 = {}
-        # split data sets and tokenize
-        ## train/test split
-        #tokenized_dataset = tokenize(dataset, tokenizer)
-        #ds_dict['train'], ds_dict['test'] = train_test_split(dataset, test_size=size_test,train_size=size_train,shuffle=True)
-        ds_dict = dataset.train_test_split(test_size=size_test,train_size=size_train,shuffle=True)
-        
-        training_sets.append(ds_dict['train'])
-        #validation_sets.append(ds_dict_2['test'])
-        test_sets.append(ds_dict['test'])
-
-        # combined test set
-        #ds_dict_2['train'], ds_dict_2['test'] = train_test_split(ds_dict['test'],train_size=COMBINED_RATIO,shuffle=True)
-        ds_dict_2 = ds_dict['test'].train_test_split(train_size=COMBINED_RATIO,shuffle=True)
-        if combined_test_set is None:
-            combined_test_set = ds_dict_2['train']
-        else:
-            #combined_test_set = pd.concat([combined_test_set,ds_dict_2['train']])
-            combined_test_set = concatenate_datasets([combined_test_set,ds_dict_2['train']])
-    test_sets.append(combined_test_set)
-
-    path_combined_test = path_datasets / 'combined_test'
-    Path(path_combined_test).mkdir(parents=True, exist_ok=True)
-    #test_sets.append(path_combined_test)
-    combined_test_set.save_to_disk(path_combined_test)
-
-    # train and evaluate classifiers
-    for i in tqdm(range(len(data_sets))):
-        path_model = path_models / "{}_{}_model".format(str(i),dataset_names[i])
-        #train_dataset = transform_to_dataset(training_sets[i], tokenizer)
-        # get train, test dataloader
-        #train_dataloader = DataLoader(train_dataset, batch_size = 5, shuffle = False)
-        model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
-        #optimizer = AdamW(model.parameters(), lr=3e-5)
-        #model, train_dataloader, optimizer = accelerator.prepare(model, train_dataloader, optimizer)
-        train_dataset = training_sets[i].map(tokenize, batched=True, batch_size=len(training_sets[i]))
-        train_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
-        # define trainer
-        training_args = TrainingArguments(
-            output_dir=path_model,          # output directory
-            num_train_epochs=num_epochs,              # total # of training epochs
-            per_device_train_batch_size=batch,  # batch size per device during training
-            per_device_eval_batch_size=64,   # batch size for evaluation
-            #warmup_steps=500,                # number of warmup steps for learning rate scheduler
-            weight_decay=0.01,               # strength of weight decay
-            logging_dir=path_logs
-        )
-
-        trainer = Trainer(
-            model=model,                         # the instantiated 🤗 Transformers model to be trained
-            args=training_args,                  # training arguments, defined above
-            train_dataset=train_dataset,         # training dataset
-            compute_metrics=compute_metrics,
-            #tokenizer = tokenizer
-        )
-        
-        # train model
-        trainer.train()  
-        
-        # evaluate model
-        accuracy = []
-        f1 = []
-        precision = []
-        recall = []
-        #predictions = []
+    for run_iter in range(number_of_runs):
+        # split data into train and test set  
+        training_sets = []
+        validation_sets = []
+        test_sets = []
+        combined_test_set = None
+        for i,dataset in enumerate(data_sets):
+            ds_dict = {}
+            ds_dict_1 = {}
+            ds_dict_2 = {}
+            # split data sets and tokenize
+            ## train/test split
+            #tokenized_dataset = tokenize(dataset, tokenizer)
+            #ds_dict['train'], ds_dict['test'] = train_test_split(dataset, test_size=size_test,train_size=size_train,shuffle=True)
+            ds_dict = dataset.train_test_split(test_size=size_test,train_size=size_train,shuffle=True)
             
-        for j in range(len(test_sets)):
-            # prepare evluation test set
-            # eval_dataset = test_sets[j].map(tokenize, batched=True, batch_size=len(train_dataset))
-            # eval_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
-            eval_dataset = test_sets[j].map(tokenize, batched=True, batch_size=len(train_dataset))
-            eval_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
-            
-            # predict
-            results_it = trainer.predict(eval_dataset) 
-            accuracy.append(results_it.metrics["test_accuracy"])
-            f1.append(results_it.metrics['test_f1'])
-            precision.append(results_it.metrics['test_precision'])
-            recall.append(results_it.metrics['test_recall'])
-            #predictions.append(results)
+            training_sets.append(ds_dict['train'])
+            #validation_sets.append(ds_dict_2['test'])
+            test_sets.append(ds_dict['test'])
 
-            
-        results = {}
-        results['f1'] = f1
-        results['precision'] = precision
-        results['recall'] = recall
-        results['accuracy'] = accuracy
-        #results['predictions'] = predictions
-            
-        # save model
-        trainer.save_model(path_model)
+            # combined test set
+            #ds_dict_2['train'], ds_dict_2['test'] = train_test_split(ds_dict['test'],train_size=COMBINED_RATIO,shuffle=True)
+            ds_dict_2 = ds_dict['test'].train_test_split(train_size=COMBINED_RATIO,shuffle=True)
+            if combined_test_set is None:
+                combined_test_set = ds_dict_2['train']
+            else:
+                #combined_test_set = pd.concat([combined_test_set,ds_dict_2['train']])
+                combined_test_set = concatenate_datasets([combined_test_set,ds_dict_2['train']])
+        test_sets.append(combined_test_set)
 
-        # with path_output.open('wb') as fp:
-        #     pickle.dump(results, fp)
-        pickle.dump(results, open("{}{}_{}.pkl".format(path_output,str(i),dataset_names[i]), "wb"))
+        path_combined_test = path_datasets / 'combined_test'
+        Path(path_combined_test).mkdir(parents=True, exist_ok=True)
+        #test_sets.append(path_combined_test)
+        combined_test_set.save_to_disk(path_combined_test)
 
-        # clear GPU memory
-        del model, trainer
-        gc.collect()
+        # train and evaluate classifiers
+        for i in tqdm(range(len(data_sets))):
+            path_model = path_models / "{}_{}_model".format(str(i),dataset_names[i])
+            #train_dataset = transform_to_dataset(training_sets[i], tokenizer)
+            # get train, test dataloader
+            #train_dataloader = DataLoader(train_dataset, batch_size = 5, shuffle = False)
+            model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+            #optimizer = AdamW(model.parameters(), lr=3e-5)
+            #model, train_dataloader, optimizer = accelerator.prepare(model, train_dataloader, optimizer)
+            train_dataset = training_sets[i].map(tokenize, batched=True, batch_size=len(training_sets[i]))
+            train_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
+            # define trainer
+            training_args = TrainingArguments(
+                output_dir=path_model,          # output directory
+                num_train_epochs=num_epochs,              # total # of training epochs
+                per_device_train_batch_size=batch,  # batch size per device during training
+                per_device_eval_batch_size=64,   # batch size for evaluation
+                #warmup_steps=500,                # number of warmup steps for learning rate scheduler
+                weight_decay=0.01,               # strength of weight decay
+                logging_dir=path_logs
+            )
+
+            trainer = Trainer(
+                model=model,                         # the instantiated 🤗 Transformers model to be trained
+                args=training_args,                  # training arguments, defined above
+                train_dataset=train_dataset,         # training dataset
+                compute_metrics=compute_metrics,
+                #tokenizer = tokenizer
+            )
+            
+            # train model
+            trainer.train()  
+            
+            # evaluate model
+            accuracy = []
+            f1 = []
+            precision = []
+            recall = []
+            #predictions = []
+                
+            for j in range(len(test_sets)):
+                # prepare evluation test set
+                # eval_dataset = test_sets[j].map(tokenize, batched=True, batch_size=len(train_dataset))
+                # eval_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
+                eval_dataset = test_sets[j].map(tokenize, batched=True, batch_size=len(train_dataset))
+                eval_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
+                
+                # predict
+                results_it = trainer.predict(eval_dataset) 
+                accuracy.append(results_it.metrics["test_accuracy"])
+                f1.append(results_it.metrics['test_f1'])
+                precision.append(results_it.metrics['test_precision'])
+                recall.append(results_it.metrics['test_recall'])
+                #predictions.append(results)
+
+                
+            results = {}
+            results['f1'] = f1
+            results['precision'] = precision
+            results['recall'] = recall
+            results['accuracy'] = accuracy
+            #results['predictions'] = predictions
+                
+            # # save model
+            # trainer.save_model(path_model)
+
+            pickle.dump(results, open("{}{}{}_{}.pkl".format(path_output,str(run_iter),str(i),dataset_names[i]), "wb"))
+
+            # clear GPU memory
+            del model, trainer
+            gc.collect()
 
     evaluation_results = []
-    for i in range(len(data_sets)):
-        file = "{}{}_{}.pkl".format(path_output,str(i),dataset_names[i])
-        single_result = pickle.load(open(file, "rb"))
-        evaluation_results.append(single_result)
+    for r in range(number_of_runs):
+        for i in range(len(data_sets)):
+            file = "{}{}{}_{}.pkl".format(path_output,str(run_iter),str(i),dataset_names[i])
+            single_result = pickle.load(open(file, "rb"))
+            evaluation_results.append(single_result)
     now = datetime.now()
-    results_dir = "./results/"+"cross-dataset_performance_"+now.strftime("%Y%m%d-%H%M%S")+"/"
+    results_dir = "./results/"+"avg-cross-dataset_performance_"+now.strftime("%Y%m%d-%H%M%S")+"/"
     if os.path.exists(results_dir) == False:
         os.makedirs(results_dir)
     print("Macro F1")
-    plotMatrix(evaluation_results,dataset_names, results_dir,selected_type="f1")
+    plotMatrix(number_of_runs, evaluation_results,dataset_names, results_dir,selected_type="f1")
     print("Precision")
-    plotMatrix(evaluation_results,dataset_names, results_dir,selected_type="precision")
+    plotMatrix(number_of_runs, evaluation_results,dataset_names, results_dir,selected_type="precision")
     print("Recall")
-    plotMatrix(evaluation_results,dataset_names, results_dir,selected_type="recall")
+    plotMatrix(number_of_runs, evaluation_results,dataset_names, results_dir,selected_type="recall")
     print("Accuracy")
-    plotMatrix(evaluation_results,dataset_names, results_dir,selected_type="accuracy")
+    plotMatrix(number_of_runs, evaluation_results,dataset_names, results_dir,selected_type="accuracy")
 
     
