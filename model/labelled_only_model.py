34985f899e9865b69e35f96bad7a1cb2e6165ae7
Markus <MarkusBertram@marbertram96@gmail.com>
2022-07-27 22:18:41 +0200

    added inference to labelled only model

--- model/labelled_only_model.py
+++ model/labelled_only_model.py
@@ -27,3 +27,13 @@ class labelled_only_model(nn.Module):
         class_output = self.task_classifier(feature_extractor_output)
 
         return class_output
+    
+    # inference for testing
+    def inference(self, input_data):
+        bert_output = self.bert(input_ids=input_data[:,0], attention_mask=input_data[:,1], return_dict = False, output_hidden_states=self.output_hidden_states)
+        
+        feature_extractor_output = self.feature_extractor(bert_output)
+
+        class_output = self.task_classifier(feature_extractor_output)
+
+        return class_output
