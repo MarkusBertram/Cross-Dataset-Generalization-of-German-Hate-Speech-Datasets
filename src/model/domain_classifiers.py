aec475df83a38320a8ccc0a46fd75a0598d979be
Markus <MarkusBertram@marbertram96@gmail.com>
2022-09-11 13:45:41 +0200

    added layer params to domain classifier

--- src/model/domain_classifiers.py
+++ src/model/domain_classifiers.py
@@ -5,16 +5,16 @@ from torch.nn import CrossEntropyLoss
 import gc
 
 class DANN_domain_classifier(nn.Module):
-    def __init__(self):
+    def __init__(self, bottleneck_dim = 768, layer_size = 100):
         super().__init__()
         
         self.domain_classifier = nn.Sequential()
-        self.domain_classifier.add_module('d_fc1', nn.Linear(768, 100))
+        self.domain_classifier.add_module('d_fc1', nn.Linear(bottleneck_dim, layer_size))
         self.domain_classifier.add_module('d_relu1', nn.ReLU())
         self.domain_classifier.add_module('d_drop1', nn.Dropout())
-        self.domain_classifier.add_module('d_fc2', nn.Linear(100, 100))
+        self.domain_classifier.add_module('d_fc2', nn.Linear(layer_size, layer_size))
         self.domain_classifier.add_module('d_relu2', nn.ReLU())
-        self.domain_classifier.add_module('d_fc2', nn.Linear(100, 1))
+        self.domain_classifier.add_module('d_fc2', nn.Linear(layer_size, 1))
         self.domain_classifier.add_module('d_flatten', nn.Flatten(start_dim = 0))
 
     def forward(
