7d8d646f062634e44e2f48b282b2cee07408708c
Markus <MarkusBertram@marbertram96@gmail.com>
2022-10-02 17:15:31 +0200

    adjusted single source experiment, multiple targets

--- src/experiments/experiment_single_source.py
+++ src/experiments/experiment_single_source.py
@@ -87,7 +87,7 @@ class experiment_single_source(experiment_base):
                 loss.backward()
                 self.optimizer.step()
 
-            self.writer.add_scalar(f"total_loss/train/{self.exp_name}", total_loss, epoch)
+            # self.writer.add_scalar(f"total_loss/train/{self.exp_name}", total_loss, epoch)
 
     def create_optimizer(self) -> None:
         self.optimizer = optim.Adam(
@@ -102,9 +102,16 @@ class experiment_single_source(experiment_base):
     # overrides load_settings
     def load_exp_settings(self) -> None:
         self.source_name = self.current_experiment.get("source_name", "germeval2018")
-        self.target_name = self.current_experiment.get("target_name", "telegram_gold")
+        self.targets = self.current_experiment.get("targets", [
+              "germeval2018", 
+              "germeval2019",
+              "hasoc2019",
+              "hasoc2020",
+              "ihs_labelled",
+              "covid_2021"
+          ])
         self.fair = self.current_experiment.get("fair", False)
-        self.exp_name = f"single_source_{self.source_name}_{self.target_name}_fair_{str(self.fair)}"
+        
         self.lr = self.current_experiment.get("lr", 5.2e-5)
         self.beta1 = self.current_experiment.get("beta1", 0.875)
         self.beta2 = self.current_experiment.get("beta2", 0.945)
@@ -150,9 +157,44 @@ class experiment_single_source(experiment_base):
             train_size = 0.8
             test_size = 0.2
 
-        source_features_train, source_features_test, source_labels_train, source_labels_test = train_test_split(source_tokens_array, source_labels_array, train_size = train_size, test_size = test_size, random_state=self.seed, stratify = source_labels_array)
+        source_features_train, _, source_labels_train, _ = train_test_split(source_tokens_array, source_labels_array, train_size = train_size, test_size = test_size, random_state=self.seed, stratify = source_labels_array)
 
         train_dataset = TensorDataset(torch.from_numpy(source_features_train), torch.from_numpy(source_labels_train).float())
+
+        # create train dataloader
+        sampler = BatchSampler(RandomSampler(train_dataset), batch_size=self.batch_size, drop_last=False)
+        self.train_dataloader = DataLoader(dataset=train_dataset, sampler = sampler, num_workers=self.num_workers)                        
+
+    def create_test_dataloader(self):
+        ####### fetch source dataset
+
+        # fetch datasets
+        label2id = {"neutral": 0, "abusive":1}
+        # import dataset pipeline
+        source_module = fetch_import_module(self.source_name)
+        # execute get_data_binary in pipeline
+        dset_list_of_dicts = source_module.get_data_binary()
+        # convert list to dataframe
+        dset_df = pd.DataFrame(dset_list_of_dicts)
+
+        # tokenize each row in dataframe
+        tokens_df = dset_df.apply(lambda row: self.preprocess(row.text), axis='columns', result_type='expand')
+
+        source_tokens_array = np.array(tokens_df[["input_ids", "attention_mask"]].values.tolist())
+        #map neutral to 0 and abusive to 1
+        source_label_df = dset_df["label"].map(label2id)
+        source_labels_array = np.array(source_label_df.values.tolist())
+
+        # "fair" is the scenario where all datasets have the same training and test size
+        # "unfair" is the scenario where 80% of each total dataset is used for train, 20% for test
+        if self.fair == True:
+            train_size = 919
+            test_size = 230
+        else:
+            train_size = 0.8
+            test_size = 0.2
+
+        _, source_features_test, _, source_labels_test = train_test_split(source_tokens_array, source_labels_array, train_size = train_size, test_size = test_size, random_state=self.seed, stratify = source_labels_array)
 
         if self.source_name == self.target_name:
             test_dataset = TensorDataset(torch.from_numpy(source_features_test), torch.from_numpy(source_labels_test).float())
@@ -172,17 +214,13 @@ class experiment_single_source(experiment_base):
             target_label_df = target_dset_df["label"].map(label2id)
             target_labels_array = np.array(target_label_df.values.tolist())
             
-            target_features_train, target_features_test, target_labels_train, target_labels_test = train_test_split(target_tokens_array, target_labels_array, train_size=train_size, test_size = test_size, random_state=self.seed, stratify = target_labels_array)
+            _, target_features_test, _, target_labels_test = train_test_split(target_tokens_array, target_labels_array, train_size=train_size, test_size = test_size, random_state=self.seed, stratify = target_labels_array)
 
             test_dataset = TensorDataset(torch.from_numpy(target_features_test), torch.from_numpy(target_labels_test).float())
-
-        # create train dataloader
-        sampler = BatchSampler(RandomSampler(train_dataset), batch_size=self.batch_size, drop_last=False)
-        self.train_dataloader = DataLoader(dataset=train_dataset, sampler = sampler, num_workers=self.num_workers)            
-
+        
         # create test dataloader
         sampler = BatchSampler(RandomSampler(test_dataset), batch_size=self.batch_size, drop_last=False)
-        self.test_dataloader = DataLoader(dataset=test_dataset, sampler = sampler, num_workers=self.num_workers)               
+        self.test_dataloader = DataLoader(dataset=test_dataset, sampler = sampler, num_workers=self.num_workers) 
 
     # ovlossides perform_experiment
     def perform_experiment(self):
@@ -206,12 +244,19 @@ class experiment_single_source(experiment_base):
 
         # perform train
         self.train()
-        
-        # perform test
-        self.test()
 
-        if self.save_model == True:
-            path_state_dict = Path(f"./model_state_dicts/{self.exp_name}")
-            path_state_dict.mkdir(parents=True, exist_ok=True)
-            torch.save(self.model.state_dict(), path_state_dict)
+        for t_name in self.targets:
+            self.target_name = t_name
+            self.exp_name = f"single_source_{self.source_name}_{self.target_name}_fair_{str(self.fair)}"
+            
+            # fetch datasets and create dataloader
+            self.create_test_dataloader()
+
+            # perform test
+            self.test()
+
+            if self.source_name == self.target_name and self.save_model == True:
+                path_state_dict = Path(f"./model_state_dicts/{self.exp_name}")
+                path_state_dict.mkdir(parents=True, exist_ok=True)
+                torch.save(self.model.state_dict(), str(path_state_dict)+f"/{self.exp_name}_model.pt")
 
